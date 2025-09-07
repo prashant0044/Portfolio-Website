cec43fa2a078f154b69548d91ad175080cb88ca3
Prashant Rana <ranaprashant282@gmail.com>
2025-09-07 19:51:14 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar"
